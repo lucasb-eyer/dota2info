f50bd01a3e60f44c50359ed0f1436f53979e71ba
Vladislav Supalov <V.Supalov@googlemail.com>
2012-11-30 16:01:38 +0100

    New scripts, combatlog translation transfered to a helperfunction. FactionConflict in the making.

--- dota2info/helpers.py
+++ dota2info/helpers.py
@@ -1,9 +1,26 @@
 import json
 
 class SummaryEncoder(json.JSONEncoder):
-""" This class is used to convert the Player object to a json dict """
+    """ This class is used to convert the Player object to a json dict """
     def default(self, obj):
        if isinstance(obj, Player):
           return obj.get_dict()
        return json.JSONEncoder.default(self, obj)
 
+def getCombatlogsTranslated(replay):
+    """ assumes the replay consists of linewise json.loads 
+    products, from the raw .demson
+
+    filters for gameevent combatlogs, 
+    replaces all ids by strings from the stringtable 
+    """
+    #The last replay combatlog stringtable. (which is complete)
+    tr = filter(lambda ge: ge['demsontype'] == 'stringtable' and \
+        ge['tablename'] == "CombatLogNames", replay)[-1]['stringtable']
+    # Note: tr stands for translate.
+    to_tr = ('attackername', 'inflictorname', 'sourcename', 'targetname', 'targetsourcename')
+
+    combatlogs = filter(lambda ge: ge['demsontype'] == 'gameevent' and ge['evname'] == 'dota_combatlog', replay)
+    combatlogs_tr = map(lambda ge: {k: tr[v] if k in to_tr else v for k, v in ge.iteritems()}, combatlogs)
+
+    return combatlogs_tr
--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(name='Distutils',
       version='0.1',
       packages=['dota2info'],
       package_data={"dota2info":[]},
-      scripts=["scripts/dota2info_summary"]
+      scripts=["scripts/dota2info_summary",
+               "scripts/dota2info_combatlog",
+               "scripts/dota2info_factionConflict"]
       )
